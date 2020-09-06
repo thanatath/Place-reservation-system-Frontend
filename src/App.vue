2068513a5a7a93d49a2fdfe55c2248616294fbea
SHIBASAN <62015048@kmitl.ac.th>
2020-09-07 06:51:09 +0700

    Set Markup

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <!-- Nav Bar Zone-->
+    <nav class="container navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <img
+            src="./assets/logo.svg"
+            width="30"
+            height="30"
+            alt=""
+          />
+        </a>
+      </nav>
+      <a class="navbar-brand kmitlfont" href="#">หน้าหลัก</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#"
+              >รายการสถานที่ <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ค้นหาสถานที่</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              ระบบสมาชิก
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">สมัครสมาชิก</a>
+              <a class="dropdown-item" href="#">ข้อมูลสมาชิก</a>
+              <a class="dropdown-item" href="#">รายการจอง</a>
+              <a class="dropdown-item" href="#">ออกจากระบบ</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <!-- Search Bar Zone-->
+      <form class="form-inline my-2 my-lg-0">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="ค้นหาสถานที่"
+          aria-label="Search"
+        />
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          ค้นหา
+        </button>
+      </form>
+      <!-- End Search Bar Zone-->
+    </nav>
+    <!-- End Nav Bar Zone-->
+ 
+
+ 
+    <router-view/>
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,246 @@
+<template>
+  <!-- Content -->
+  <div class="container">
+    <div class="row" style="margin: 1%">
+      <div class="col-sm-8">
+        <div class="alert kmitlc" role="alert">
+          <h3 class="text-center">จองสถานที่</h3>
+        </div>
+        <!-- Picker Zone-->
+        <div class="row">
+          <!-- Left Picker Zone-->
+          <div class="col-sm">
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <label class="input-group-text" for="inputGroupSelect01"
+                  >ประเภทสถานที่</label
+                >
+              </div>
+              <select
+                class="custom-select"
+                id="inputGroupSelect01"
+                v-model="ptype"
+                @change="updatefindtext()"
+              >
+                <option value="อาคาร">อาคาร</option>
+                <option value="ลานกว้าง">ลานกว้าง</option>
+                <option value="ห้อง">ห้อง</option>
+              </select>
+            </div>
+          </div>
+          <!-- Right Picker Zone-->
+          <div class="col-sm-6">
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <label class="input-group-text" for="inputGroupSelect01"
+                  >รองรับคน</label
+                >
+              </div>
+              <select
+                @change="updatefindtext()"
+                v-model="pmax"
+                class="custom-select"
+                id="inputGroupSelect01"
+              >
+                <option value="ไม่เกิน 10 คน">ไม่เกิน 10 คน</option>
+                <option value="ไม่เกิน 50 คน">ไม่เกิน 50 คน</option>
+                <option value="ไม่เกิน 100 คน">ไม่เกิน 100 คน</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <!-- End Picker Zone-->
+        <!-- Second Picker -->
+        <div class="row justify-content-between">
+          <!-- Type Zone -->
+          <div class="col-sm-6">
+            <div class="card" style="width: auto">
+              <div class="card-header">ประเภทสถานที่ที่จะเข้าใช้งาน</div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input
+                        v-model="paccess"
+                        @change="updatefindtext()"
+                        type="radio"
+                        class="form-check-input"
+                        name="req"
+                        value="ต้องขออนุญาต"
+                      />ต้องขออนุญาต
+                    </label>
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input
+                        v-model="paccess"
+                        @change="updatefindtext()"
+                        type="radio"
+                        class="form-check-input"
+                        name="req"
+                        value="ไม่ต้องขออนุญาต"
+                      />ไม่ต้องขออนุญาต
+                    </label>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- End Type Zone -->
+          <!-- Device Zone -->
+          <div class="col-sm-6">
+            <div class="card" style="width: auto">
+              <div class="card-header">อุปกรณ์</div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                  <label class="checkbox-bootstrap">
+                    <input
+                      value="โปรเจ็กเตอร์"
+                      type="checkbox"
+                      v-model="devices"
+                      @change="updatefindtext()"
+                    />
+                    <span class="checkbox-placeholder"></span>
+                    โปรเจ็กเตอร์
+                  </label>
+                </li>
+                <li class="list-group-item">
+                  <label class="checkbox-bootstrap">
+                    <input
+                      value="จอภาพ"
+                      type="checkbox"
+                      v-model="devices"
+                      @change="updatefindtext()"
+                    />
+                    <span class="checkbox-placeholder"></span>
+                    จอภาพ
+                  </label>
+                </li>
+                <li class="list-group-item">
+                  <label class="checkbox-bootstrap">
+                    <input
+                      value="เครื่องเสียง"
+                      type="checkbox"
+                      v-model="devices"
+                      @change="updatefindtext()"
+                    />
+                    <span class="checkbox-placeholder"></span>
+                    เครื่องเสียง
+                  </label>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- End Device Zone -->
+        </div>
+        <!-- Bottom Zone -->
+        <div class="row" style="margin-top: 2%">
+          <div class="col-sm-6">
+            <span id="find" class="badge badge-primary">สนใจค้นหา</span>
+
+            <span style="white-space: pre-wrap; " >{{ findtext }}</span>
+          </div>
+          <div class="col-sm-6">
+            <button type="button" class="btn btn-lg btn-block kmitlc">
+              ค้นหา
+            </button>
+          </div>
+        </div>
+        <!-- End Bottom Zone -->
+      </div>
+      <!-- Right Zone -->
+      <div style="margin-top:10px"  class="col-sm-4">
+        <div class="row">
+          <div class="col">
+            <div
+              id="carouselExampleSlidesOnly"
+              class="carousel slide"
+              data-ride="carousel"
+            >
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img
+                    class="d-block w-100"
+                    src="https://e-file.huawei.com/-/media/EBG/Images/case-study/cases_study_banner/How%20Did%20Huawei%20Build%20the%20100G%20SDN%20Campus%20Network%20for%20KMITL%20of%20Thailand-revised-banner-list.jpg"
+                    alt="First slide"
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src="https://static.trueplookpanya.com/tppy/member/m_525000_527500/525016/cms/images/%E0%B8%A0%E0%B8%B2%E0%B8%9E%E0%B8%9B%E0%B9%89%E0%B8%B2%E0%B8%A2%20I%20love%20KMITL%20(2).jpg"
+                    alt="Second slide"
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src="https://miro.medium.com/max/8064/1*ja89Odmw_LV4zXB_5u66OQ@2x.jpeg"
+                    alt="Third slide"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- News -->
+        <div class="row">
+          <div class="col">
+            <h5 class="text-center">
+              <span class="badge badge-pill badge-warning kmitlc"
+                >แจ้งข่าวสาร</span
+              >
+            </h5>
+            <ul class="list-group">
+              <li class="list-group-item">ข่าว 1</li>
+              <li class="list-group-item">ข่าว 2</li>
+              <li class="list-group-item">ข่าว 3</li>
+            </ul>
+          </div>
+        </div>
+        <!--End News -->
+      </div>
+    </div>
+  </div>
+
+  <!-- End Right Zone -->
+  <!-- End Content -->
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    updatefindtext() {
+      this.findtext =
+        ' สถานที่ประเภท' +
+        this.ptype +
+        ' ที่สามารถรองรับคนได้' +
+        this.pmax;
+      if (this.paccess !== '') {
+        this.findtext = this.findtext + ' โดยเป็นสถานที่ที่' + this.paccess;
+      }
+      if (Object.keys(this.devices).length !== 0) {
+        this.findtext = this.findtext + ' และประสงค์ใช้ ' + this.devices;
+      }
+    },
+  },
+  data() {
+    return {
+      ptype: '', //ประเภทสถานที่
+      pmax: '', //จำนวนรองรับ
+      paccess: '', //การขออนุญาตใช้สถานที่
+      devices: [],
+      testarr: ['1', '2', '3'],
+      findtext: '',
+    };
+  },
+  mounted() {
+    (this.ptype = 'อาคาร'),
+      (this.pmax = 'ไม่เกิน 10 คน'),
+      this.updatefindtext();
+  },
+};
+</script>
